509d876e86091caa6e6133a0ba120678cefba11c
LibertoBianchi <lbianchi@cifpfbmoll.eu>
2021-11-30 17:17:00 +0100

    Actualizacion Login SySTEM

--- Ejercicios/php-login-script-level-1/login.php
+++ Ejercicios/php-login-script-level-1/login.php
@@ -63,7 +63,36 @@ include_once "layout_head.php";
  
 echo "<div class='col-sm-6 col-md-4 col-md-offset-4'>";
  
-    // alert messages will be here
+    
+// get 'action' value in url parameter to display corresponding prompt messages
+$action=isset($_GET['action']) ? $_GET['action'] : "";
+ 
+// tell the user he is not yet logged in
+if($action =='not_yet_logged_in'){
+    echo "<div class='alert alert-danger margin-top-40' role='alert'>Please login.</div>";
+}
+ 
+// tell the user to login
+else if($action=='please_login'){
+    echo "<div class='alert alert-info'>
+        <strong>Please login to access that page.</strong>
+    </div>";
+}
+ 
+// tell the user email is verified
+else if($action=='email_verified'){
+    echo "<div class='alert alert-success'>
+        <strong>Your email address have been validated.</strong>
+    </div>";
+}
+ 
+// tell the user if access denied
+if($access_denied){
+    echo "<div class='alert alert-danger margin-top-40' role='alert'>
+        Access Denied.<br /><br />
+        Your username or password maybe incorrect
+    </div>";
+}
  
     // actual HTML login form
     echo "<div class='account-wall'>";
