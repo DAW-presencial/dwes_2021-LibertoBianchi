5e91c8d16ca620435fa113c290b35bc98631ab01
LibertoBianchi <lbianchi@cifpfbmoll.eu>
2021-12-14 17:26:49 +0100

    prueba psql

--- Ejercicios/agendaDB/agenda_bd_Bianchi_Bleda_Liberto/pdo/Conexion.php
+++ Ejercicios/agendaDB/agenda_bd_Bianchi_Bleda_Liberto/pdo/Conexion.php
@@ -17,8 +17,7 @@ function conectar(){
     */
     try{
         
-    $this->conexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,
-    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $this->conexion = new PDO("pgsql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
 
     $this->conexion->query("CREATE DATABASE IF NOT EXISTS agenda_db");
     $this->conexion->query("USE agenda_db");
