07faecbeb63b9b9387c6e2da829269fd8f770ed4
LibertoBianchi <lbianchi@cifpfbmoll.eu>
2021-12-14 16:52:12 +0100

    prueba adminer

--- Ejercicios/agendaDB/agenda_bd_Bianchi_Bleda_Liberto/pdo/Conexion.php
+++ Ejercicios/agendaDB/agenda_bd_Bianchi_Bleda_Liberto/pdo/Conexion.php
@@ -2,7 +2,7 @@
  define('DB_HOST','localhost');
  define('DB_USER','root');
  define('DB_PASS','');
- define('DB_NAME','agenda_db');
+ define('DB_NAME','agenda_lbianchi');
 
 class Conexion{
    public $conexion;
